2fe9976d5ab02047557b2cab2f19835591e85673
zuma990 <zumajuve@gmail.com>
2022-04-04 12:44:23 +0200

    Added new file

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+// Write code below this line
+
+function convertStrToInt(str) {
+    return parseInt(str);
+}
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+
+
+// Write code above this line
+
+module.exports = convertStrToInt;
