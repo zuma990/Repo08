0bd233fee9bd34648972d88fae99393435f17fb7
zuma990 <zumajuve@gmail.com>
2022-04-04 12:14:02 +0200

    Added new file

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+var total = 0;
+function funcWithArg(digit) {
+return (digit + 9) / 5;
+}
+total = funcWithArg(6);
+
+// Write code above this line
+console.log(total);
+module.exports = funcWithArg;
