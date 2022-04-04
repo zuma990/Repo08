ea24bc1cc5c8c230cdde2239d23d91a94ff62b53
zuma990 <zumajuve@gmail.com>
2022-04-04 12:31:39 +0200

    Added new file

---
+++ ex09/src/index.js
@@ -0,0 +1,8 @@
+// Write code below this line
+function randomInteger() {
+    return Math.floor(Math.random() * 30);
+}
+console.log(randomInteger());
+
+// Write code above this line
+module.exports = randomInteger;
