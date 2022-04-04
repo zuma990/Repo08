0b8a9dd65767ad4845280333ec91624aea201e7e
zuma990 <zumajuve@gmail.com>
2022-04-04 10:38:59 +0200

    Added new file

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+// Write code below this line
+
+function localScope(){
+    var myVariable = "I am here!";
+    console.log(myVariable);
+}
+localScope();
+if(typeof myVariable != "undefined") {
+    console.log('outside localScope', myVariable);
+}
+else {
+    console.log('outside localScope UNDEFINED!!!');
+    }
+
+module.exports = localScope;    
+
+// Write code above this line
