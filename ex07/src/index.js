5530447dc004c94a8c12280380b61f14f016fd29
zuma990 <zumajuve@gmail.com>
2022-04-04 12:05:36 +0200

    Added new file

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+var dress = "little dress"; 
+
+function myDress() {
+    var dress = "ball gown";
+    return dress;
+}
+console.log(myDress());
+// Write code above this line
+module.exports =myDress;
