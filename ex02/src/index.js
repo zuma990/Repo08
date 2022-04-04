8f5986e5d68e3f9e32a05412c111454ab6e6f15b
zuma990 <zumajuve@gmail.com>
2022-04-04 10:17:40 +0200

    Added new file

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+// Write code below this line
+
+function functionPrintInfo(name, age){
+    
+    console.log(name, age);
+}
+
+// Write code above this line
+
+module.exports = functionPrintInfo("John", 23);
+module.exports = functionPrintInfo("Michael", 43);
